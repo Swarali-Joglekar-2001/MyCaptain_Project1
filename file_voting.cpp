689b1e6bdeeaa2f1a75534ae1b902e74dc3036f4
Swarali-Joglekar-2001 <74260706+Swarali-Joglekar-2001@users.noreply.github.com>
2020-11-15 19:15:57 +0530

    Create file_voting.cpp
    Commiting a new file containing program to check voting eligibility.

---
+++ file_voting.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+int main()
+{
+
+int age;
+char m,n;
+std::cout<<"Enter your age : ";
+std::cin>>age;
+if(age>=18)
+{
+
+std::cout<<"Do you have residence proof? Type Y if you have it; else type N :\n";
+std::cin>>m;
+std::cout<<"\nDo you have identity proof? Type Y if you have it; else type N :\n";
+std::cin>>n;
+	if(m=='Y'&&n=='Y')
+	{
+	
+		std::cout<<"\nYou are eligible for voting.";
+	}
+
+	else
+	{
+		
+		std::cout<<"\nYou are not eligible for voting";
+	}
+
+}
+else if(age<18)
+{
+	std::cout<<"\nYou are not eligible for voting";
+
+}
+
+}
+
+
